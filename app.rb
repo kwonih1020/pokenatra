6b2c24225efdcb6c495e5b09b6bdf8302859b0ee
Ick Hyun Kwon <kwonih1020@gmail.com>
2017-04-17 22:03:31 -0400

    done for trainer

--- app.rb
+++ app.rb
@@ -13,3 +13,39 @@ get "/" do
     @trainers = Trainer.all
     erb :"trainers/index"
 end
+
+get '/trainers/new' do
+    erb :"trainers/new"
+end
+
+get "/trainers/:id" do
+    @trainer = Trainer.find(params[:id])
+    erb :"trainers/show"
+end
+
+post "/trainers" do
+    @trainer = Trainer.create(name: params[:name], level: params[:id])
+    redirect "/trainers/#{@trainer.id}"
+end
+
+post '/trainers' do
+    @trainer = Trainer.create(params[:trainer])
+    redirect "/trainers/#{@trainer.id}"
+end
+
+get "/trainers/:id/edit" do
+    @trainer = Trainer.find(params[:id])
+    erb (:"trainers/edit")
+end
+
+put "/trainers/:id" do
+    @trainer = Trainer.find(params[:id])
+    @trainer.update(params[:trainer])
+    redirect ("/trainers/#{@trainer.id}")
+end
+
+delete "/trainers/:id" do
+    @trainer = Trainer.find(params[:id])
+    @trainer.destroy
+    redirect "/"
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 
 require 'active_record'
-# require 'ffaker'
+require 'ffaker'
 require 'pg'
 require_relative 'connection'
 
